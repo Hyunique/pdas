d40d0817edeee5cfaae4be0260052767a8a7eaac
Elena <kunstvanelena@gmail.com>
2023-08-25 14:57:32 +0200

    add bulma, style cards

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import "bulma/css/bulma.css";
 import ProfileCard from "./ProfileCard";
 import AlexaImage from "./images/alexa.png";
 import CortanaImage from "./images/cortana.png";
@@ -6,10 +7,44 @@ import SiriImage from "./images/siri.png";
 function App() {
   return (
     <div>
-      <div>Personal Digital Assistants</div>
-      <ProfileCard title="Alexa" handle="@alexa99" imgSrc={AlexaImage} />
-      <ProfileCard title="Cortana" handle="@cortana32" imgSrc={CortanaImage} />
-      <ProfileCard title="Siri" handle="@siri01" imgSrc={SiriImage} />
+      <section className="hero is-primary">
+        <div className="hero-body">
+          <p className="title">Personal Digital Assistants</p>
+        </div>
+      </section>
+
+      <div className="container">
+        <section className="section">
+          <div className="columns">
+            <div className="column is-3">
+              <ProfileCard
+                title="Alexa"
+                handle="@alexa99"
+                imgSrc={AlexaImage}
+                description="Alexa was created by Amazon and helps you buy things"
+              />
+            </div>
+            <div className="column is-3">
+              {" "}
+              <ProfileCard
+                title="Cortana"
+                handle="@cortana32"
+                imgSrc={CortanaImage}
+                description="Cortana was made by Microsoft. Who knows what is does?"
+              />
+            </div>
+            <div className="column is-3">
+              {" "}
+              <ProfileCard
+                title="Siri"
+                handle="@siri01"
+                imgSrc={SiriImage}
+                description="Siri was made by Apple and is being phased out"
+              />
+            </div>
+          </div>
+        </section>
+      </div>
     </div>
   );
 }
--- src/ProfileCard.js
+++ src/ProfileCard.js
@@ -1,11 +1,22 @@
-function ProfileCard({ title, handle, imgSrc }) {
+function ProfileCard({ title, handle, imgSrc, description }) {
   // const { title, handle } = props;
   return (
-    <div>
-      <img src={imgSrc} alt="{title}logo" />
-      <div>Title is {title}</div>
-      <div>Handle is {handle}</div>
-    </div>
+    <>
+      <div className="card">
+        <div className="card-image">
+          <figure className="image is-1by1">
+            <img src={imgSrc} alt="{title}logo" />
+          </figure>
+        </div>
+        <div className="card-content">
+          <div className="media-content">
+            <p className="title is-4"> {title}</p>
+            <p className="subtitle is-6"> {handle}</p>
+          </div>
+          <div className="content">{description}</div>
+        </div>
+      </div>
+    </>
   );
 }
 
